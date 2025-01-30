59a8b6a9c04e4eba70a6032d910d827fa3ad528e
Dean Benjamin <dben1182@gmail.com>
2025-01-29 17:50:49 -0700

    autopilot works

--- parameters/anaconda_parameters.py
+++ parameters/anaconda_parameters.py
@@ -11,7 +11,7 @@ from tools.rotations import euler_to_quaternion
 #initial conditions for the QUADPLANE
 pn0 = 0.  # initial north position
 pd0 = 0.  # initial down position
-u0 = 0.  # initial velocity along body x-axis
+u0 = 20.  # initial velocity along body x-axis
 w0 = 0.  # initial velocity along body z-axis
 theta0 = 0.  # initial pitch angle
 q0 = 0.  # initial pitch rate
