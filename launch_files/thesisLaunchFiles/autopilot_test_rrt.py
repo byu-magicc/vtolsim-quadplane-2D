9f3cda30dd6ddf292417a99bba2e79a8603908bb
Dean Anderson <dben1182@gmail.com>
2026-02-26 13:23:16 -0700

    working on RRT and tracking

--- launch_files/thesisLaunchFiles/autopilot_test_rrt.py
+++ launch_files/thesisLaunchFiles/autopilot_test_rrt.py
@@ -27,6 +27,7 @@ from rrt_mavsim.message_types.msg_plane import MsgPlane
 from message_types.msg_trajectory import MsgTrajectory
 from rrt_mavsim.parameters.colors import *
 from rrt_mavsim.tools.plane_projections_2 import map_2D_to_3D, map_3D_to_2D
+from rrt_mavsim.viewers.plot_map_path import PlotMapPath
 
 #import section for the rrt stuff
 from rrt_mavsim.message_types.msg_world_map import MsgWorldMap
@@ -36,8 +37,6 @@ from rrt_mavsim.message_types.msg_world_map import MsgWorldMap, PlanarVTOLSimpli
 from eVTOL_BSplines.path_generation_helpers.staticFlightPath import staticFlightPath
 from bsplinegenerator.bsplines import BsplineEvaluation
 from planners.trajectoryGenerator import trajectoryGenerator
-
-
 
 #section to create the planar VTOL map
 #creates the path generator
@@ -51,7 +50,8 @@ path_gen = RRT_SFC_BSpline(numDimensions=SFC.numDimensions,
                            plane=CONDA.plane_msg,
                            chiMax=SFC.Chi_max)
 
-planar_params = PlanarVTOLSimplifiedParams(plane=CONDA.plane_msg)
+planar_params = PlanarVTOLSimplifiedParams(plane=CONDA.plane_msg,)
+
 world_map = MsgWorldMap(obstacleFieldType=MapTypes.PLANAR_VTOL_SIMPLIFIED,
                         numDimensions_algorithm=SFC.numDimensions,
                         planarVTOLSimplified_Params=planar_params)
@@ -81,5 +81,67 @@ waypoints_smooth = path_gen.getWaypointsSmooth()
 #draws the waypoints, starting with the waypoints_not_smooth
 viewers.drawWaypoints(waypoints=waypoints_not_smooth, color='blue')
 viewers.drawWaypoints(waypoints=waypoints_smooth, color='purple')
+
+#creates the control points
+controlPoints_notSmooth_2D = path_gen.generateControlPoints(waypoints=waypoints_not_smooth,
+                                                         numPointsPerUnit=0.01)
+
+#evaluates the B-Spline for the not smooth control points
+bspline_notSmooth = BsplineEvaluation(control_points=controlPoints_notSmooth_2D,
+                                      order=3,
+                                      start_time=0.0)
+#samples the not smooth bspline
+splineSampledPoints_notSmooth_2D, timeData = bspline_notSmooth.get_spline_data(num_data_points_per_interval=100)
+
+#maps the 2D points to 3D
+controlPoints_notSmooth_3D = map_2D_to_3D(vec_2D=controlPoints_notSmooth_2D,
+                                          plane=CONDA.plane_msg)
+splineSampledPoints_notSmooth_3D = map_2D_to_3D(vec_2D=splineSampledPoints_notSmooth_2D,
+                                                plane=CONDA.plane_msg)
+
+#draws the not smooth trajecotry
+viewers.drawTrajectory(controlPoints=controlPoints_notSmooth_3D,
+                       sampledPoints_spline=splineSampledPoints_notSmooth_3D,
+                       lineColor=np.array([[1.,1.,0.,1.],
+                                           [1.,1.,0.,1.]]),
+                       lineWidth=2.0,
+                       pointWidth=5.0)
+
+#creates the control points
+controlPoints_smooth_2D = path_gen.generateControlPoints(waypoints=waypoints_smooth,
+                                                         numPointsPerUnit=0.01)
+
+#evaluates the B-Spline for the not smooth control points
+bspline_smooth = BsplineEvaluation(control_points=controlPoints_smooth_2D,
+                                      order=3,
+                                      start_time=0.0)
+#samples the not smooth bspline
+splineSampledPoints_smooth_2D, timeData = bspline_smooth.get_spline_data(num_data_points_per_interval=100)
+
+#maps the 2D points to 3D
+controlPoints_smooth_3D = map_2D_to_3D(vec_2D=controlPoints_smooth_2D,
+                                          plane=CONDA.plane_msg)
+splineSampledPoints_smooth_3D = map_2D_to_3D(vec_2D=splineSampledPoints_smooth_2D,
+                                                plane=CONDA.plane_msg)
+
+#draws the not smooth trajecotry
+viewers.drawTrajectory(controlPoints=controlPoints_smooth_3D,
+                       sampledPoints_spline=splineSampledPoints_smooth_3D,
+                       lineColor=np.array([[1.,156.0/255.0,0.,1.],
+                                           [1.,156.0/255.0,0.,1.]]),
+                       lineWidth=2.0,
+                       pointWidth=5.0)
+
+
+# instantiates the quadplane
+quadplane = QuadplaneDynamics(
+    ts=SIM.ts_simulation,
+    plane_msg=CONDA.plane_msg,
+    pos_3D_inertial_init=RRT.startPosition_3D,
+    vel_3D_inertial_init=startVelocity_3D,
+)
+
+
+
 
 testPoint = 0
--- parameters/rrt_test_param.py
+++ parameters/rrt_test_param.py
@@ -4,7 +4,7 @@ import parameters.anaconda_parameters as CONDA
 from rrt_mavsim.tools.plane_projections_2 import map_3D_to_2D
 
 startPosition_3D = np.array([[0.0],[0.0],[0.0]])
-endPosition_3D = np.array([[1000.0],[0.0],[-1000.0]])
+endPosition_3D = np.array([[2000.0],[0.0],[-1000.0]])
 
 startPosition_2D = map_3D_to_2D(vec_3D=startPosition_3D,
                                 plane=CONDA.plane_msg)
